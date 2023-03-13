ca5e535a020ba56c83eb0d216cb3727c2d252982
Gareth Jones <java.jones@gmail.com>
2023-03-13 17:48:53 -0600

    Squashed commit of the following:
    commit 32ab73fa22eebba5a0a376430a511f5df8bcb9ae
    Author: Gareth Jones <java.jones@gmail.com>
    Date:   Mon Mar 13 17:48:22 2023 -0600
    
        ReactionTableSearch has additional table columns

--- pylib_3.9.7/df/ReactionTableSearch.py
+++ pylib_3.9.7/df/ReactionTableSearch.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional, Union, Tuple
+from typing import List, Dict, Optional, Union, Tuple, Any
 
 from rdkit import Chem
 from rdkit.Chem.rdChemReactions import ChemicalReaction
@@ -34,6 +34,17 @@ class ReactionTableSearch(DataFunction):
                 ds.append(diff)
             return froms, tos, ds
 
+        def carry_over(from_row: int, to_row: int, values: List[List[Optional[Any]]]) -> Tuple[
+            List[Optional[Any]], List[Optional[Any]]]:
+            froms: list[Optional[Any]] = []
+            tos: list[Optional[Any]] = []
+            for p in values:
+                p1 = p[from_row]
+                p2 = p[to_row]
+                froms.append(p1)
+                tos.append(p2)
+            return froms, tos
+
         def mol_to_smiles(mol: Mol) -> Optional[str]:
             try:
                 mol = Chem.RemoveHs(mol)
@@ -53,13 +64,18 @@ class ReactionTableSearch(DataFunction):
 
         structure_column_id = string_input_field(request, 'structureColumn')
         structures = column_to_molecules(request.inputColumns[structure_column_id])
+        structure_to_row: Dict[str, int] = {mol_to_smiles(mol): r for r, mol in enumerate(structures) if mol}
         id_column_id = string_input_field(request, 'idColumn')
         id_column = request.inputColumns[id_column_id]
         ids: List[Union[str, int]] = id_column.values
         property_column_ids = string_list_input_field(request, 'propertyColumns')
-        structure_to_row: Dict[str, int] = {mol_to_smiles(mol): r for r, mol in enumerate(structures) if mol}
         properties = [request.inputColumns[column_id].values for column_id in property_column_ids]
         property_names = ['_'.join(request.inputColumns[column_id].name.split()) for column_id in property_column_ids]
+        additional_column_ids = string_list_input_field(request, 'additionalColumns')
+        additional_values = [request.inputColumns[column_id].values for column_id in additional_column_ids]
+        additional_names = ['_'.join(request.inputColumns[column_id].name.split()) for column_id in
+                            additional_column_ids]
+        additional_columns = [request.inputColumns[column_id] for column_id in additional_column_ids]
 
         lhs: List[Mol] = []
         rhs: List[Mol] = []
@@ -68,11 +84,15 @@ class ReactionTableSearch(DataFunction):
         deltas: List[List[Optional[float]]] = []
         from_properties: List[List[Optional[float]]] = []
         to_properties: List[List[Optional[float]]] = []
+        from_values: List[List[Optional[Any]]] = []
+        to_values: List[List[Optional[Any]]] = []
         for _ in range(len(property_names)):
             deltas.append([])
             from_properties.append([])
             to_properties.append([])
-            from_properties.append([])
+        for _ in range(len(additional_columns)):
+            from_values.append([])
+            to_values.append([])
 
         for row, structure in enumerate(structures):
             if not structure:
@@ -111,6 +131,11 @@ class ReactionTableSearch(DataFunction):
                         from_properties[i].append(v)
                     for i, v in enumerate(t):
                         to_properties[i].append(v)
+                    f, t = carry_over(row, other_row, additional_values)
+                    for i, v in enumerate(f):
+                        from_values[i].append(v)
+                    for i, v in enumerate(t):
+                        to_values[i].append(v)
 
         lhs_column = molecules_to_column(lhs, 'LHS', DataType.BINARY)
         rhs_column = molecules_to_column(rhs, 'RHS', DataType.BINARY)
@@ -124,6 +149,15 @@ class ReactionTableSearch(DataFunction):
             columns.append(column)
             column = ColumnData(name=f'Delta {name}', dataType=DataType.DOUBLE, values=deltas[index])
             columns.append(column)
+        for index, name in enumerate(additional_names):
+            additional_column = additional_columns[index]
+            column = ColumnData(name=f'LHS {name}', dataType=additional_column.dataType,
+                                values=from_values[index], contentType=additional_column.contentType)
+            columns.append(column)
+            column = ColumnData(name=f'RHS {name}', dataType=additional_column.dataType,
+                                values=to_values[index], contentType=additional_column.contentType)
+            columns.append(column)
+
         output_table = TableData(tableName='Pair Finder Results', columns=columns)
         response = DataFunctionResponse(outputTables=[output_table])
         return response
