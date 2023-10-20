2e6b8c3cfc65cc2633699cb4253aada0ad44e909
Kirk DeLisle <kdelisle@glysade.com>
2023-10-20 15:09:51 -0600

    Make sequence column identification generic

--- pylib_3.9.7/df/BlastLocalColumnSearch.py
+++ pylib_3.9.7/df/BlastLocalColumnSearch.py
@@ -12,12 +12,25 @@ class BlastLocalColumnSearch(DataFunction):
 
     def execute(self, request: DataFunctionRequest) -> DataFunctionResponse:
         sequence_column_id = string_input_field(request, 'sequenceColumn')
-        id_column_id = string_input_field(request, 'idColumn')
+
+        # when marking/filtering is enabled, the name column ID
+        # is suffixed with _both, _filteredRows, _markedRow_<Marking ID>, or _both_<Marking ID>
+        if sequence_column_id not in request.inputColumns.keys():
+            for column_id in request.inputColumns.keys():
+                if sequence_column_id in column_id:
+                    sequence_column_id = column_id
+
         sequence_column = request.inputColumns[sequence_column_id]
+
+        id_column_id = string_input_field(request, 'idColumn')
         id_column = None if id_column_id is None else request.inputColumns[id_column_id]
+
         input_sequences = column_to_sequences(sequence_column, id_column)
         input_sequences = [s for s in input_sequences if s]
+
         blastdb = string_input_field(request, 'blastDbPath')
         os.environ['BLASTDB'] = blastdb
-        return run_blast_search(input_sequences, request, 'Blast column search results', sequence_column.contentType,
+
+        return run_blast_search(input_sequences, request, 'Blast column search results',
+                                sequence_column.contentType,
                                 True)
