102e497eeb9559c6e2f186c084322e9d9cbddd80
Kirk DeLisle <kdelisle@glysade.com>
2024-01-15 17:07:21 -0700

    Complete implementation of ExtractGenebankRegions

--- pylib_3.9.7/df/ExtractGenbankRegions.py
+++ pylib_3.9.7/df/ExtractGenbankRegions.py
@@ -5,8 +5,8 @@ from Bio.SeqFeature import SeqFeature
 from Bio.SeqRecord import SeqRecord
 
 from df.bio_helper import column_to_sequences, sequences_to_column
-from df.data_transfer import DataFunction, DataFunctionRequest, DataFunctionResponse, \
-                             TableData, integer_input_field, string_input_field
+from df.data_transfer import DataFunction, DataFunctionRequest, DataFunctionResponse, DataType, \
+                             ColumnData, TableData, integer_input_field, string_input_field, input_field_to_column
 
 
 def _feature_to_name(feature: SeqFeature, feature_key: str, feature_qualifier: str) -> Optional[str]:
@@ -26,10 +26,11 @@ def _feature_to_name(feature: SeqFeature, feature_key: str, feature_qualifier: s
 
 
 def _extract_region(sequence: Optional[SeqRecord], feature_key: str, feature_qualifier: str, name: str) \
-        -> Optional[Seq]:
+        -> [Optional[Seq]]:
     if not sequence:
         return None
 
+    feature_sequences = []
     feature: SeqFeature
     for feature in sequence.features:
         feature_name = _feature_to_name(feature, feature_key, feature_qualifier)
@@ -44,9 +45,11 @@ def _extract_region(sequence: Optional[SeqRecord], feature_key: str, feature_qua
                 # need to create a notification here
                 continue
 
-            return SeqRecord(seq, name)
+            # return SeqRecord(seq, name)
+            feature_sequences.append(SeqRecord(seq, name, features = [feature]))
 
-    return None
+    return feature_sequences
+    # return None
 
 
 class ExtractGenbankRegions(DataFunction):
@@ -58,18 +61,28 @@ class ExtractGenbankRegions(DataFunction):
         input_column = next(iter(request.inputColumns.values()))
         input_column.remove_nulls()
         input_sequences = column_to_sequences(input_column)
+
+        sequence_ids = input_field_to_column(request, 'uiIDColumn')
+        if sequence_ids:
+            sequence_id_column_name = sequence_ids.name
+            sequence_ids_no_nulls = [seq_id for idx, seq_id in enumerate(sequence_ids.values)
+                                     if idx not in input_column.missing_null_positions]
+            # create fake ids for any missing ids
+            sequence_ids_no_nulls = [seq_id if seq_id else f'Row {i + 1}' for i, seq_id in enumerate(sequence_ids_no_nulls)]
+        else:
+            # create fake ids if no ID column was selected
+            sequence_id_column_name = 'ID'
+            sequence_ids_no_nulls = [f'Row {i + 1}' for i in range(len(input_sequences))]
+
         max_number_of_regions = integer_input_field(request, 'maximumNumberRegions')
+
         feature_key = string_input_field(request, 'featureKey')
         feature_qualifier = string_input_field(request, 'featureQualifier')
-        if not feature_qualifier:
-            generate_table = True
-        else:
-            generate_table = False
 
         # first find all regions - dict preserves order
         region_names_dict: Dict[str] = {}
         finished = False  # finished is never changed to True
-        for sequence in input_sequences:
+        for sequence, sequence_id in zip(input_sequences, sequence_ids_no_nulls):
             if finished:
                 break  # this may never run since finished is never True
             if not sequence:
@@ -86,21 +99,55 @@ class ExtractGenbankRegions(DataFunction):
 
         region_names = list(region_names_dict.keys())
         output_columns = []
+        feature_sequence_ids = []
 
         for region_name in region_names:
             feature_sequences = [_extract_region(s, feature_key, feature_qualifier, region_name)
                                  for s in input_sequences]
-            output_column = sequences_to_column(feature_sequences, f'{input_column.name} {region_name}', False)
+            feature_sequence_ids = [i for i, seq in zip(sequence_ids_no_nulls, input_sequences) if seq]
+
 
             if feature_qualifier:
+                # verify handling of list of lists
+                output_column = sequences_to_column([feature_sequence[0] if len(feature_sequence) != 0 else None
+                                                     for feature_sequence in feature_sequences],
+                                        f'{input_column.name} {region_name}', False)
                 output_column.insert_nulls(input_column.missing_null_positions)
+                output_columns.append(output_column)
 
-            output_columns.append(output_column)
+                response = DataFunctionResponse(outputColumns = output_columns)
+            else:
+                output_feature_sequences = []
+                output_feature_ids = []
+                output_feature_descr = []
+                output_feature_start = []
+                output_feature_end = []
+                for feature_seqs, sequence_id in zip(feature_sequences, feature_sequence_ids):
+                    output_feature_ids.extend([sequence_id] * len(feature_seqs))
+                    output_feature_sequences.extend(feature_seqs)
 
-        if feature_qualifier:
-            response = DataFunctionResponse(outputColumns = output_columns)
-        else:
-            output_table = TableData(tableName = f'{input_column.name} {region_name}',
-                                     columns = output_columns)
-            response = DataFunctionResponse(outputTables = [output_table])
+                    for feature_seq in feature_seqs:
+                        qualifiers = [v for k, v in feature_seq.features[0].qualifiers.items()]
+                        output_feature_descr.append(qualifiers[0][0])
+                        output_feature_start.append(feature_seq.features[0].location.start)
+                        output_feature_end.append(feature_seq.features[0].location.end)
+                        # output_feature_start.extend([feature_seq.features[0].location.start for feature_seq in feature_seqs])
+                        # output_feature_end.extend([feature_seq.features[0].location.end for feature_seq in feature_seqs])
+
+                output_columns = [
+                                  ColumnData(name = sequence_id_column_name, dataType = DataType.STRING,
+                                             values = output_feature_ids),
+                                  sequences_to_column(output_feature_sequences, f'{input_column.name} {region_name}', False),
+                                  ColumnData(name = 'Description', dataType = DataType.STRING,
+                                             values = output_feature_descr),
+                                  ColumnData(name = 'Start', dataType = DataType.INTEGER,
+                                             values = output_feature_start),
+                                  ColumnData(name = 'end', dataType = DataType.INTEGER,
+                                             values = output_feature_end)
+                ]
+
+                output_table = TableData(tableName = f'{input_column.name} {feature_key}', columns = output_columns)
+
+                response = DataFunctionResponse(outputTables = [output_table])
+
         return response
